fecac010c1ee9167bf8a54c18c4b6daccbd234a3
Maksim Drachov <maksim.drachov@outlook.com>
2021-01-28 12:28:06 +0100

    Stable 800khz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -100,7 +100,7 @@ int main(void)
   MX_TIM2_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim2);
+  //HAL_TIM_Base_Start_IT(&htim2);
   HAL_TIM_Base_Start_IT(&htim3);
 
   /* USER CODE END 2 */
